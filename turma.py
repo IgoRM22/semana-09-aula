3abe2987a330f46c28780d5ded72329c8e6356c6
Igor <i.ramos@aluno.ifsp.edu.br>
2024-12-05 22:10:23 -0300

    Correção

--- turma.py
+++ turma.py
@@ -1,18 +1,20 @@
 class Turma:
-  def __init__(self):
-    self.turma = [];
-    self.menorNota = None;
-    self.maiorNota = None;
+    def __init__(self):
+        self.turma = []
+        self.menorNota = None
+        self.maiorNota = None
 
-  def cadastrarAlunos(self, alunos):    
-    for i in alunos:
-      self.turma.append(i);
-      if((self.menorNota == None) or (self.menorNota.nota > i.nota)):
-        self.menorNota = i;
-      elif((self.maiorNota == None) or (self.menorNota.nota < i.nota)):
-        self.maiorNota = i;                 
+    def cadastrarAlunos(self, alunos):
+        for aluno in alunos:
+            if aluno.nota < 0 or aluno.nota > 10:
+                raise ValueError(f"Nota inválida: {aluno.nota}. Deve estar entre 0 e 10.")
+            self.turma.append(aluno)
+            if self.menorNota is None or aluno.nota < self.menorNota.nota:
+                self.menorNota = aluno
+            if self.maiorNota is None or aluno.nota > self.maiorNota.nota:
+                self.maiorNota = aluno
 
-  def mostrarAlunos(self):  
-    print('Quantidade de alunos:', len(self.turma));
-    for x in self.turma:      
-      print(x.mostrarAluno());
+    def mostrarAlunos(self):
+        print('Quantidade de alunos:', len(self.turma))
+        for aluno in self.turma:
+            print(aluno.mostrarAluno())
